6f2a5873409dec4b8a95541531c8d1a6c9d502f6
Ali Mesbahi <55492850+90rimane@users.noreply.github.com>
2023-11-10 09:04:08 +0100

    Update server.js
    some changes added

--- server.js
+++ server.js
@@ -15,17 +15,10 @@ app.use(express.static("public"));
 // Parse JSON bodies
 app.use(bodyParser.json());
 
-// // Klarna Real API credentials
-// const klarnaCredentials = {
-//   username: "K6738509_a8dc33e4db35",
-//   password: "jVQolEKIxg3ygWED",
-// };
-// const klarnaApiUrl = "https://api.klarna.com";
-
-const klarnaCredentials = {
-  username: 'PK133431_6aea19f53b6f',
-  password: 'qOTr0DD72ENxq5nu',
-};
+//const klarnaCredentials = {
+  //username: 'PK133431_6aea19f53b6',
+  //password: 'qOTr0DD72ENxq5nu',
+//};
 const klarnaApiUrl = 'https://api.playground.klarna.com';
 
 // Route to create Klarna session
